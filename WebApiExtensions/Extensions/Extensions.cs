d17d10092ad92ad6fa2167ea354201450dfe11e6
chaowlert <chaowlert@gmail.com>
2016-01-02 04:02:06 +0700

    add Extensions & ExcludeModelValidator

---
+++ WebApiExtensions/Extensions/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dependencies;
+
+public static class Extensions
+{
+    public static T GetService<T>(this IDependencyResolver resolver)
+    {
+        return (T)resolver.GetService(typeof(T));
+    }
+    
+    public static string ToQueryString(this NameValueCollection data)
+    {
+        var strs = data.Cast<string>()
+            .Select(name => string.Concat(name, "=", HttpUtility.UrlEncode(data[name])));
+        return string.Join("&", strs);
+    }
+    
+    public static HttpResponseException ToException(this HttpResponseMessage response)
+    {
+        return new HttpResponseException(response);
+    }
+
+}
---
+++ WebApiExtensions/Services/ExcludeModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Validation;
+
+namespace WebApiExtensions.Services
+{
+    public class ExcludeModelValidator : DefaultBodyModelValidator
+    {
+        public readonly HashSet<Type> ExcludeTypes = new HashSet<Type>(); 
+
+        public override bool ShouldValidateType(Type type)
+        {
+            return !ExcludeTypes.Contains(type) && base.ShouldValidateType(type);
+        }
+    }
+}
